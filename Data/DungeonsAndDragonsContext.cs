7cdb02366a05f7171eea112589d1c4a2d38cbca5
stanchyyy <stanchyyy@gmail.com>
2021-08-12 01:17:53 +0300

    - added player and data classes;

---
+++ Data/DungeonsAndDragonsContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DungeonsAndDragons.Data
+{
+    public class DungeonsAndDragonsContext : DbContext
+    {
+
+        public DbSet<DungeonsAndDragons.Player> Player { get; set; }
+    }
+}
---
+++ Models/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DungeonsAndDragons
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string PlayerName { get; set; }
+        public string Email { get; set; }
+        public bool Alive { get; set; }
+        public int PageProgress { get; set; }
+
+    }
+}
