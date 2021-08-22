0319513eec9b9b9b3cf010c070dd39563c76c23d
stanchyyy <stanchyyy@gmail.com>
2021-08-23 01:37:34 +0300

    added data validation for email and username

--- Models/Player.cs
+++ Models/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,10 @@ namespace DungeonsAndDragons
     public class Player
     {
         public int Id { get; set; }
+        [Required]
         public string PlayerName { get; set; }
+        [EmailAddress]
+        [Required]
         public string Email { get; set; }
         public bool Alive { get; set; }
         public int PageProgress { get; set; }
