d3eb27f60568b968d3bd1ecd4276c9b51bdc783e
stanchyyy <stanchyyy@gmail.com>
2021-08-25 09:12:12 +0300

    added property binding to the player class

--- Models/Player.cs
+++ Models/Player.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace DungeonsAndDragons
 {
+    [Index(nameof(Email), IsUnique = true)]
     public class Player
     {
         public int Id { get; set; }
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,6 +14,7 @@ namespace DungeonsAndDragons.Pages
     {
         private readonly ILogger<IndexModel> _logger;
         private readonly Data.DungeonsAndDragonsContext _context;
+
 
         public IndexModel(ILogger<IndexModel> logger, Data.DungeonsAndDragonsContext context)
         {
@@ -26,16 +29,40 @@ namespace DungeonsAndDragons.Pages
 
 
         [BindProperty]
+        public string PlayerSearch { get; set; }
+
+
+
+        public IActionResult OnPostButtonSignIn(string playerSearch)
+        {
+            {
+
+                PlayerSearch = playerSearch;
+                var TryFindPlayer = from player in _context.Player where player.Email == PlayerSearch select player;
+                Player = TryFindPlayer.FirstOrDefault();
+                //Search using direct sql
+                //var searchAgain = _context.Player.FromSqlRaw("SELECT * FROM dbo.Player where ").ToList();
+                if (Player is null)
+                {
+                    ModelState.AddModelError("PlayerSearch","Player not registered, please sign up.");
+                    return Page();
+                }
+                else return RedirectToPage("./Welcome", Player);
+
+            }
+        }
+        [BindProperty]
         public Player Player { get; set; }
 
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostButtonSignUp()
         {
             if (!ModelState.IsValid)
             {
                 return Page();
             }
+
             Player.Alive = true;
-            Player.PageProgress= 1;
+            Player.PageProgress = 1;
             _context.Player.Add(Player);
             await _context.SaveChangesAsync();
             return RedirectToPage("./Welcome", Player);
